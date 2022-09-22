24abc90cc2035908fb9b8d7a000644b4b8495ee7
Maycon Cardoso <maycon.santos.cardoso@gmail.com>
2022-09-22 15:18:44 +0100

    Home Container

--- app/src/main/java/com/mctech/pokergrinder/HomeActivity.kt
+++ app/src/main/java/com/mctech/pokergrinder/HomeActivity.kt
@@ -2,6 +2,9 @@ package com.mctech.pokergrinder
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -10,5 +13,17 @@ class HomeActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_home)
+    setUpBottomNavigation()
+  }
+
+  override fun onSupportNavigateUp(): Boolean {
+    return getNavigationController().navigateUp()
+  }
+
+  private fun getNavigationController() = Navigation.findNavController(this, R.id.feature_fragment)
+
+  private fun setUpBottomNavigation() {
+    val bottomNav: BottomNavigationView = findViewById(R.id.bottom_navigation_main)
+    NavigationUI.setupWithNavController(bottomNav, getNavigationController())
   }
 }
