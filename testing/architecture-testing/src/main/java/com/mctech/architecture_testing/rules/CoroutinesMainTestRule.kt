c74d65daa7ffda8f324e4129ba65f95a4a9b454b
Maycon Cardoso <maycon.santos.cardoso@gmail.com>
2022-12-13 17:57:11 +0000

    Adding jacoco

--- testing/architecture-testing/src/main/java/com/mctech/architecture_testing/rules/CoroutinesMainTestRule.kt
+++ testing/architecture-testing/src/main/java/com/mctech/architecture_testing/rules/CoroutinesMainTestRule.kt
@@ -2,21 +2,23 @@ package com.mctech.architecture_testing.rules
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.TestCoroutineDispatcher
 import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.setMain
 import org.junit.runner.Description
 
+@OptIn(ExperimentalCoroutinesApi::class)
 class CoroutinesMainTestRule constructor(
-  val testDispatcher: TestCoroutineDispatcher = TestCoroutineDispatcher()
+  private val testDispatcher: TestCoroutineDispatcher = TestCoroutineDispatcher()
 ) : InstantTaskExecutorRule() {
 
-  override fun starting(description: Description?) {
+  override fun starting(description: Description) {
     super.starting(description)
     Dispatchers.setMain(testDispatcher)
   }
 
-  override fun finished(description: Description?) {
+  override fun finished(description: Description) {
     super.finished(description)
     Dispatchers.resetMain()
     testDispatcher.cleanupTestCoroutines()
