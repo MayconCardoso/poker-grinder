8816beaafe468b3c0df5e911329e1e406806ae7a
Maycon Cardoso <maycon.santos.cardoso@gmail.com>
2022-12-14 15:49:10 +0000

    Adding jacoco

--- features/summary/presentation/pager_container/src/main/java/com/mctech/pokergrinder/summary/presentation/pager_container/SummaryContainerFragment.kt
+++ features/summary/presentation/pager_container/src/main/java/com/mctech/pokergrinder/summary/presentation/pager_container/SummaryContainerFragment.kt
@@ -6,9 +6,7 @@ import androidx.fragment.app.Fragment
 import com.google.android.material.tabs.TabLayoutMediator
 import com.mctech.pokergrinder.architecture.extensions.viewBinding
 import com.mctech.pokergrinder.summary.presentation.pager_container.databinding.FragmentSummaryContainerBinding
-import dagger.hilt.android.AndroidEntryPoint
 
-@AndroidEntryPoint
 public class SummaryContainerFragment : Fragment(R.layout.fragment_summary_container) {
 
   private val binding by viewBinding(FragmentSummaryContainerBinding::bind)
