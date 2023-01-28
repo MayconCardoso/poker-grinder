1f950ef464249aaf1cfd946f63a31cf86701dd98
Maycon Cardoso <maycon.santos.cardoso@gmail.com>
2023-01-28 18:48:06 +0000

    [Feature] Ranges Viewer Presentation Structure

--- features/ranges/domain/src/main/java/com/mctech/pokergrinder/ranges/domain/RangesRepository.kt
+++ features/ranges/domain/src/main/java/com/mctech/pokergrinder/ranges/domain/RangesRepository.kt
@@ -7,6 +7,14 @@ import kotlinx.coroutines.flow.Flow
  * Transaction repository to access range data.
  */
 interface RangesRepository {
+
+  /**
+   * Used to observe all ranges.
+   */
   fun observeAllRanges(): Flow<List<Range>>
+
+  /**
+   * Used to observe an specific [range].
+   */
   fun observeRange(range: Range): Flow<Range>
 }
